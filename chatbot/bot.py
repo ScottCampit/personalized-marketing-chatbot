e22a460dc367fe573281d64e7b140b5686139891
Scott Campit <scampit@umich.edu>
2023-01-18 21:25:57 -0800

    Updated front-end to submit data to chatbot
    Updates include:
      * Changed text engine
      * Created field to upload CSV file
      * Created batch submit button
      * Created progress bar when running through dataframe for each customer
      * Created download button
    
    TODO:
      * Optimize chatbot response -> returning None

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -5,10 +5,15 @@ Fine-tune GPT models
 import os
 import openai
 import streamlit as st
+import pandas as pd
+import time
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-def generate_response(prompt:str, temperature:float=0.5, max_tokens:int=1500, engine='davinci'):
+# Chat GPT 
+def generate_response(prompt:str, temperature:float=0.1, max_tokens:int=2049, engine='text-davinci-003'):
+    """
+    """
     response = openai.Completion.create(
         engine=engine,
         prompt=prompt,
@@ -21,26 +26,72 @@ def generate_response(prompt:str, temperature:float=0.5, max_tokens:int=1500, en
     generated_text = response["choices"][0]["text"]
     return generated_text
 
-st.title("Language Translation App")
-input_text = st.text_area(label="Enter text to translate:")
+@st.cache
+def cache_df(df):
+    return df.to_csv().encode('utf-8')
 
-with st.sidebar:
-    value_prop = st.text_area(
-        label="What is your product's value proposition?",
-        value="Our product is going to change the world because of X, Y, and Z."
+if __name__ == "__main__":
+    # Upload Excel or CSV file
+    st.title("NewCo Application")
+    uploaded_file = st.file_uploader(
+        label="Upload a single CSV file containing your contacts' information.", 
+        type='csv',
+        accept_multiple_files=False
+    )
+
+    if uploaded_file is not None:
+        try:
+            df = pd.read_csv(uploaded_file)
+        except:
+            print("Wrong file type - please upload a csv.")
+            
+    # Create sidebar
+    with st.form(key='my_form'):
+        value_prop = st.text_area(
+            label="What is your product's value proposition?",
+            value="Our product is going to change the world because of X, Y, and Z."
+        )
+        contact_list_type = st.radio(
+            label="New or existing customers?",
+            options=("New", "Existing")
+        )
+        email_tone = st.radio(
+            label="What is the tone?",
+            options=("Formal", "Neutral", "Causual")
+        )
+        creative_conservative = st.radio(
+            label="How creative versus conservative would you want to be?",
+            options=("Creative", "Conservative")
+        )
+        st.write('Press submit to generate personalized emails.')
+        submit_button = st.form_submit_button(label='Submit')
+
+    # Irrelevant -> need to edit the prompt based on user input
+    is_done = False
+    if submit_button:
+        st.write("Generating emails!")
+        all_prompts = list()
+        all_responses = list()
+        bar = st.progress(0)
+        for idx, row in df.iterrows():
+            time.sleep(0.1)
+            bar.progress(idx+1)
+            prompt = f"""Write a {email_tone}, {creative_conservative} email to {row['Name']}, {row['Title']} who is a {contact_list_type} customer from {row['Company']} in {row['Location']}. 
+            {row['Name']} {row['Fictional fun fact from data source']}. Product: {value_prop},"""
+            response = generate_response(prompt)
+            all_prompts.append(prompt)
+            all_responses.append(response)
+        is_done = True
+
+        df['prompt'] = all_prompts
+        df['response'] = all_responses
+        st.write("Finished!")
     
-    )
-    email_tone = st.radio(
-        "What is the tone?",
-        ("Formal", "Neutral", "Causual")
-    )
-    creative_conservative = st.radio(
-        "How creative versus conservative would you want to be?",
-        ("Creative", "Conservative")
-    )
+    if is_done:
+        st.download_button(
+            label="Download data as CSV",
+            data=cache_df(df),
+            file_name='test.csv'
+        )
 
-st.title("Chatbot Assistant")
-prompt = st.text_input(label="Enter your message: ")
-if prompt:
-  response = generate_response(prompt)
-  st.write("Chatbot:", response)
+
